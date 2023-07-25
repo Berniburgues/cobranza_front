c3d0f536f773a922f665ffc3de0327419fe7cd16
Bernardo Burgués <burguesbernardo@gmail.com>
2023-07-25 15:33:11 -0300

    Nav

--- src/components/Common/Layout.jsx
+++ src/components/Common/Layout.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen overflow-x-auto">
-      <nav className="bg-gray-800 p-4 text-white text-lg font-semibold flex justify-between">
+      <nav className="bg-gray-800 p-4 text-white text-2xl font-semibold flex justify-between">
         <div>
           <Link to="/" className="hover:text-gray-500">
             Inicio
