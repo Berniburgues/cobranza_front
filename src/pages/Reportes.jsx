9cbb242ca69e4c1ee013e29ddd561c7b777ca127
Bernardo Burgués <burguesbernardo@gmail.com>
2023-07-31 21:46:51 -0300

    Reporte actualizado

--- src/pages/Reportes.jsx
+++ src/pages/Reportes.jsx
@@ -4,19 +4,19 @@ const Reportes = () => {
   const informes = [
     {
       title: 'Importes Enviados y Ratios',
-      url: 'https://app.powerbi.com/view?r=eyJrIjoiMTRjMmJjNTQtOWJjNC00YTYzLWEzNGItYTlkMWJkMWVmNDMzIiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSection78cce8d8350067194806',
+      url: 'https://app.powerbi.com/view?r=eyJrIjoiZTZhM2JkZDAtMDFmYy00OGI1LWEwMTYtZjI2Y2UzZWRiZGE1IiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSection78cce8d8350067194806',
     },
     {
       title: 'Cobranza por Día Hábil y Fecha Calendario',
-      url: 'https://app.powerbi.com/view?r=eyJrIjoiMTRjMmJjNTQtOWJjNC00YTYzLWEzNGItYTlkMWJkMWVmNDMzIiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSection',
+      url: 'https://app.powerbi.com/view?r=eyJrIjoiZTZhM2JkZDAtMDFmYy00OGI1LWEwMTYtZjI2Y2UzZWRiZGE1IiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSection',
     },
     {
       title: 'Importes por Banco',
-      url: 'https://app.powerbi.com/view?r=eyJrIjoiMTRjMmJjNTQtOWJjNC00YTYzLWEzNGItYTlkMWJkMWVmNDMzIiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSectionc8dfcee322ed08555074',
+      url: 'https://app.powerbi.com/view?r=eyJrIjoiZTZhM2JkZDAtMDFmYy00OGI1LWEwMTYtZjI2Y2UzZWRiZGE1IiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSectionc8dfcee322ed08555074',
     },
     {
       title: 'Códigos por Día Hábil',
-      url: 'https://app.powerbi.com/view?r=eyJrIjoiMTRjMmJjNTQtOWJjNC00YTYzLWEzNGItYTlkMWJkMWVmNDMzIiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSectione828bf90b0b55e7ed03b',
+      url: 'https://app.powerbi.com/view?r=eyJrIjoiZTZhM2JkZDAtMDFmYy00OGI1LWEwMTYtZjI2Y2UzZWRiZGE1IiwidCI6IjUxZDRjMzBhLTIzZjMtNDk5Mi04M2VkLWU4N2NhNzk0NzNmYiIsImMiOjR9&pageName=ReportSectione828bf90b0b55e7ed03b',
     },
   ];
 
