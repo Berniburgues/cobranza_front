e78254a1605b667cb16e711fd022ba3b8dabd067
Bernardo Burgués <burguesbernardo@gmail.com>
2023-11-06 17:21:56 -0300

    Importes actualizado

--- src/components/TablaPagos/ImportesSocios.jsx
+++ src/components/TablaPagos/ImportesSocios.jsx
@@ -1,46 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const ImportesSocios = ({
-  importeCobrado,
-  importeEnviadoTotal,
-  importeEnviadoTramo1,
-  importeEnviadoTramo2,
+  importes,
   altaCuiles,
   bajaCuiles,
   cuilesTotales,
   serviciosTitulares,
   serviciosAdherentes,
 }) => {
-  const [ratioTotal, setRatioTotal] = useState(0);
-  const [ratioTramo1, setRatioTramo1] = useState(0);
-  const [ratioTramo2, setRatioTramo2] = useState(0);
+  function getTramoNombre(tramoKey) {
+    const tramoNombres = {
+      'tramo 1': '0-30',
+      'tramo 2': '30-60',
+      'tramo 3': '60-90',
+      'tramo 4': '90+',
+    };
+    return tramoNombres[tramoKey] || tramoKey;
+  }
 
-  useEffect(() => {
-    // Cuando cambian los importes, actualiza el gráfico y el ratio
-    const importeEnviadoTotalNumerico = parseFloat(importeEnviadoTotal?.replace('$', ''));
-    const importeEnviadoTramo1Numerico = parseFloat(
-      importeEnviadoTramo1?.replace('$', ''),
-    );
-    const importeEnviadoTramo2Numerico = parseFloat(
-      importeEnviadoTramo2?.replace('$', ''),
-    );
-    const importeCobradoNumerico = parseFloat(importeCobrado?.replace('$', ''));
-    const newRatioTotal = (
-      (importeCobradoNumerico / importeEnviadoTotalNumerico) *
-      100
-    ).toFixed(2);
-    setRatioTotal(isNaN(newRatioTotal) ? '-' : newRatioTotal + '%');
-    const newRatioTramo1 = (
-      (importeCobradoNumerico / importeEnviadoTramo1Numerico) *
-      100
-    ).toFixed(2);
-    setRatioTramo1(isNaN(newRatioTramo1) ? '-' : newRatioTramo1 + '%');
-    const newRatioTramo2 = (
-      (importeCobradoNumerico / importeEnviadoTramo2Numerico) *
-      100
-    ).toFixed(2);
-    setRatioTramo2(isNaN(newRatioTramo2) ? '-' : newRatioTramo2 + '%');
-  }, [importeEnviadoTotal, importeEnviadoTramo1, importeEnviadoTramo2, importeCobrado]);
+  const [expandido, setExpandido] = useState(false);
+
+  const handleToggleExpandido = () => {
+    setExpandido(!expandido);
+  };
 
   return (
     <section>
@@ -66,23 +48,67 @@ const ImportesSocios = ({
           </span>
         </p>
       </article>
-      <article className="border-2 border-black rounded-md p-1">
-        <p className="text-center text-base font-semibold italic">
-          <span>Cobrado:</span>{' '}
-          <span className="font-bold italic text-green-500">{importeCobrado}</span>
-        </p>
-        <p className="text-center text-base font-semibold italic">
-          <span>Enviado 0-30:</span>{' '}
-          <span className="font-bold italic text-blue-600">{importeEnviadoTramo1}</span>{' '}
-          <span>Ratio 0-30:</span>{' '}
-          <span className="font-bold italic text-green-500">{ratioTramo1}</span>
-        </p>
-        <p className="text-center text-base font-semibold italic">
-          <span>Enviado 0-90:</span>{' '}
-          <span className="font-bold italic text-blue-600">{importeEnviadoTramo2}</span>{' '}
-          <span>Ratio 0-90:</span>{' '}
-          <span className="font-bold italic text-green-500">{ratioTramo2}</span>
-        </p>
+      <article className="rounded-md border-2 border-black mb-1 p-1">
+        {expandido && (
+          <div>
+            <p className="text-center text-base font-semibold italic">
+              <span className="underline">Enviado Total:</span>{' '}
+              <span className="font-bold text-blue-500">
+                {Number(importes.totales.enviado).toLocaleString('es-AR', {
+                  style: 'currency',
+                  currency: 'ARS',
+                  minimumFractionDigits: 2,
+                })}
+              </span>{' '}
+              | <span className="underline">Cobrado Total:</span>{' '}
+              <span className="font-bold text-green-500">
+                {Number(importes.totales.cobrado).toLocaleString('es-AR', {
+                  style: 'currency',
+                  currency: 'ARS',
+                  minimumFractionDigits: 2,
+                })}
+              </span>{' '}
+              | <span className="underline">Ratio Total:</span>{' '}
+              <span className="font-bold text-green-500">
+                {Number(importes.totales.ratio).toFixed(2)}%
+              </span>
+            </p>
+            <div className="text-center font-semibold italic">
+              {Object.keys(importes.tramos).map((tramoKey) => (
+                <p key={tramoKey}>
+                  <span className="underline">Enviado {getTramoNombre(tramoKey)}:</span>{' '}
+                  <span className="font-bold text-blue-500">
+                    {Number(importes.tramos[tramoKey].enviado).toLocaleString('es-AR', {
+                      style: 'currency',
+                      currency: 'ARS',
+                      minimumFractionDigits: 2,
+                    })}
+                  </span>{' '}
+                  | <span className="underline">Cobrado {getTramoNombre(tramoKey)}:</span>{' '}
+                  <span className="font-bold text-green-500">
+                    {Number(importes.tramos[tramoKey].cobrado).toLocaleString('es-AR', {
+                      style: 'currency',
+                      currency: 'ARS',
+                      minimumFractionDigits: 2,
+                    })}
+                  </span>{' '}
+                  | <span className="underline">Ratio {getTramoNombre(tramoKey)}:</span>{' '}
+                  <span className="font-bold text-green-500">
+                    {Number(importes.tramos[tramoKey].ratio).toFixed(2)}%
+                  </span>
+                </p>
+              ))}
+            </div>
+          </div>
+        )}
+        <div className="text-center">
+          <button
+            onClick={handleToggleExpandido}
+            className=" font-bold text-blue-600 hover:text-blue-500"
+          >
+            {expandido ? 'Ocultar Importes ⬆️' : 'Mostrar Importes ⬇️'}
+          </button>
+        </div>
       </article>
     </section>
   );
--- src/pages/TablaPagos.jsx
+++ src/pages/TablaPagos.jsx
@@ -30,10 +30,14 @@ const TablaPagos = () => {
   const [pageNumber, setPageNumber] = useState(1);
   const [showLoader, setShowLoader] = useState(true);
   const [count, setCount] = useState(0);
-  const [importeEnviadoTotal, setImporteEnviadoTotal] = useState('-');
-  const [importeEnviadoTramo1, setImporteEnviadoTramo1] = useState('-');
-  const [importeEnviadoTramo2, setImporteEnviadoTramo2] = useState('-');
-  const [importeCobrado, setImporteCobrado] = useState('-');
+  const [importes, setImportes] = useState({
+    totales: {
+      enviado: 0,
+      cobrado: 0,
+      ratio: 0,
+    },
+    tramos: {},
+  });
   const [altaCuiles, setAltaCuiles] = useState('-');
   const [bajaCuiles, setBajaCuiles] = useState('-');
   const [cuilesTotales, setCuilesTotales] = useState('-');
@@ -95,11 +99,8 @@ const TablaPagos = () => {
           key !== 'ExB',
       );
       setUniqueDates(fechasCobro);
-      const importes = await fetchImportes(selectedPeriod, selectedBanco);
-      setImporteEnviadoTotal(importes.importeEnviadoTotal);
-      setImporteEnviadoTramo1(importes.importeEnviadoTramo1);
-      setImporteEnviadoTramo2(importes.importeEnviadoTramo2);
-      setImporteCobrado(importes.importeCobrado);
+      const importesData = await fetchImportes(selectedPeriod, selectedBanco);
+      setImportes(importesData);
       const cuiles = await fetchCuiles(selectedPeriod);
       setAltaCuiles(cuiles.CuilesNuevos);
       setBajaCuiles(cuiles.CuilesBaja);
@@ -169,16 +170,20 @@ const TablaPagos = () => {
     setData([]);
     setUniqueDates([]);
     setCount(0);
-    setImporteEnviadoTotal('-');
-    setImporteEnviadoTramo1('-');
-    setImporteEnviadoTramo2('-');
-    setImporteCobrado('-');
     setBajaCuiles('-');
     setAltaCuiles('-');
     setCuilesTotales('-');
     setSelectChanges(false);
     setServiciosAdherentes('-');
     setServiciosTitulares('-');
+    setImportes({
+      totales: {
+        enviado: 0,
+        cobrado: 0,
+        ratio: 0,
+      },
+      tramos: {},
+    });
   };
 
   const startIndex = (pageNumber - 1) * pageSize;
@@ -307,10 +312,7 @@ const TablaPagos = () => {
         {showLoader ? null : (
           <section>
             <ImportesSocios
-              importeCobrado={importeCobrado}
-              importeEnviadoTotal={importeEnviadoTotal}
-              importeEnviadoTramo1={importeEnviadoTramo1}
-              importeEnviadoTramo2={importeEnviadoTramo2}
+              importes={importes}
               altaCuiles={altaCuiles}
               bajaCuiles={bajaCuiles}
               cuilesTotales={cuilesTotales}
