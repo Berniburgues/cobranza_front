8230e31d57aad12a0cfdb93bc9633de05ec5f958
Bernardo Burgués <burguesbernardo@gmail.com>
2024-03-15 16:47:56 -0300

    Ratio Bancos agregado a reportes parciales

--- src/components/Reportes/ReportesParciales.jsx
+++ src/components/Reportes/ReportesParciales.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const ReportesIndirectos = () => {
   const informesParciales = [
     {
+      title: 'RATIOS BANCOS',
+      url: 'https://app.powerbi.com/view?r=eyJrIjoiNDVjNWQ4MWUtOWY3NS00OThhLTg0NmYtYTJmYjIxYjAzMzJhIiwidCI6IjJiMmUxNjVjLTY3NDItNDY3NC05ZWJmLTkxY2QwYTcwMWE4ZiIsImMiOjR9',
+    },
+    {
       title: 'PERÍODO ATSAPRA',
       url: 'https://app.powerbi.com/view?r=eyJrIjoiYjQ4ZTY0ZWItYmU3ZC00M2YyLWEzMjMtOWJjNmQ2MDhhNjlhIiwidCI6IjJiMmUxNjVjLTY3NDItNDY3NC05ZWJmLTkxY2QwYTcwMWE4ZiIsImMiOjR9&pageName=ReportSectionf06324290e1bb7bd427b',
     },
